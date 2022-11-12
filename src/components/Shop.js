8f826fbed3ab98eb9f6ec90251aff54aebe26f02
tbruner <trevor.a.bruner@gmail.com>
2022-11-12 17:58:47 -0500

    Change class to className in Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,9 +4,9 @@ const Shop = () => {
   return (
     <>
     <h1>Hello Shop</h1>
-      <div class='catagory'>
-        <h2 class='catagory-title'>Catagory</h2>
-        <div class='card'>
+      <div className='catagory'>
+        <h2 className='catagory-title'>Catagory</h2>
+        <div className='card'>
           <p>Shoe Name</p>
         </div>
       </div>
