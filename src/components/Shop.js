557638af40316f3cce41a4e42ed3f4690e049758
tbruner <trevor.a.bruner@gmail.com>
2022-11-12 17:46:36 -0500

    Add return statement to Shop component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,15 +1,17 @@
 import '../styles/Shop.css';
 
 const Shop = () => {
-  <>
-  <h1>Hello Shop</h1>
-    <div class='catagory'>
-      <h2 class='catagory-title'>Catagory</h2>
-      <div class='card'>
-        <p>Shoe Name</p>
+  return (
+    <>
+    <h1>Hello Shop</h1>
+      <div class='catagory'>
+        <h2 class='catagory-title'>Catagory</h2>
+        <div class='card'>
+          <p>Shoe Name</p>
+        </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
 
 export default Shop;
