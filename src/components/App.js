86bee2d638d8bf6ba2fcb336575c02fcbb0c90b2
tbruner <trevor.a.bruner@gmail.com>
2022-11-12 17:19:29 -0500

    Add routes to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+import Home from './Home';
+import Shop from './Shop';
 import '../styles/App.css';
 
 function App() {
   return (
     <>
+      <Navigation />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/mens-shop' element={<Shop />} />
+          <Route path='/womens-shop' element={<Shop />} />
+          <Route path='/kids-shop' element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
