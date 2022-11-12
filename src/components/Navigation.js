37977f4d27d875b454ae924026d983c656772a65
tbruner <trevor.a.bruner@gmail.com>
2022-11-12 17:57:59 -0500

    Set up Navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
 import '../styles/Navigation.css';
 
 const Navigation = () => {
-
+  return (
+    <nav>
+      <ul>
+        <li className='nav-logo'>
+          <Link to='/'>Logo tbd</Link>
+        </li>
+        <li className='nav-mens'>
+          <Link to='/mens'>Mens</Link>
+        </li>
+        <li className='nav-womens'>
+          <Link to='/womens'>Womens</Link>
+        </li>
+        <li className='nav-kids'>
+          <Link to='/kids'>Kids</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
