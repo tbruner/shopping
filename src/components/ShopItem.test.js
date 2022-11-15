392b0c921126581ad4b6d7064dac8ae4fe250c71
tbruner <trevor.a.bruner@gmail.com>
2022-11-14 20:21:24 -0500

    Update tests

--- src/components/ShopItem.test.js
+++ src/components/ShopItem.test.js
@@ -1,4 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Navigation from "./ShopItem";
+import ShopItem from "./ShopItem";
+
+describe('Shopping Item', () => {
+  it('calls onClick when button is clicked', () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn();
+    render(<ShopItem onClick={onClickMock} />);
+    const button = screen.getByRole("button");
+
+    user.click(button);
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
