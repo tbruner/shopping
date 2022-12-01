6f81164dc317955e809f165eafd17d3560b1114c
tbruner <trevor.a.bruner@gmail.com>
2022-12-01 18:00:35 -0500

    Add checkout button test

--- src/components/ShopBar.test.js
+++ src/components/ShopBar.test.js
@@ -2,3 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ShopBar from "./ShopBar";
+
+describe('Shop status bar', () => {
+  it('Call onClick when checkout button is clicked', () => {
+    const user = userEvent.setup();
+    const onClickMock = jest.fn();
+    render(<ShopBar onClick={onClickMock} />);
+    const button = screen.getByRole('button');
+
+    user.click(button);
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
