1bd5bac68e4c2632a1bc1ef6ecb31016a849ac43
tbruner <trevor.a.bruner@gmail.com>
2022-12-01 19:21:31 -0500

    Add ShopBar to Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,10 @@
 import '../styles/Shop.css';
+import ShopBar from './ShopBar';
 
 const Shop = () => {
   return (
     <>
+    <ShopBar />
     <h1>Hello Shop</h1>
       <div className='catagory'>
         <h2 className='catagory-title'>Catagory</h2>
