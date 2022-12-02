5e46f13e40718302a1b1c2dba7ccf5a5b86eb4b3
tbruner <trevor.a.bruner@gmail.com>
2022-12-01 19:17:08 -0500

    Add ShopBar layout

--- src/components/ShopBar.js
+++ src/components/ShopBar.js
@@ -1,7 +1,11 @@
 import '../styles/ShopBar.css';
 
 const ShopBar = () => {
-
+  <div class='sticky'>
+    <span class='item-count'>Count: </span>
+    <span class='total-cost'>Total: </span>
+    <button class='checkout-btn'>Checkout</button>
+  </div>
 }
 
 export default ShopBar;
