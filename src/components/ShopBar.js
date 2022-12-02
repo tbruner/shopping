10380c8ef614a7e1debe0e69fa7cd1b5f088fd15
tbruner <trevor.a.bruner@gmail.com>
2022-12-01 19:21:20 -0500

    Add return statement to ShopBar

--- src/components/ShopBar.js
+++ src/components/ShopBar.js
@@ -1,11 +1,13 @@
 import '../styles/ShopBar.css';
 
 const ShopBar = () => {
-  <div class='sticky'>
-    <span class='item-count'>Count: </span>
-    <span class='total-cost'>Total: </span>
-    <button class='checkout-btn'>Checkout</button>
-  </div>
+  return (
+    <div class='sticky'>
+      <span class='item-count'>Count: </span>
+      <span class='total-cost'>Total: </span>
+      <button class='checkout-btn'>Checkout</button>
+    </div>
+  );
 }
 
 export default ShopBar;
