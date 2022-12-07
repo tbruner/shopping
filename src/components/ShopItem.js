b7672a809d0a679774222ddb7a05f6438a360040
tbruner <trevor.a.bruner@gmail.com>
2022-12-06 20:59:50 -0500

    Add buttons to select shoe size

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -3,10 +3,16 @@ import '../styles/ShopItem.css';
 const ShopItem = ({details}) => {
   return (
     <>
-    <span className='item-title'>{details.shoeBrand} {details.shoeName}</span>
+    <div className='item-title'>{details.shoeBrand} {details.shoeName}</div>
     <img src={details.image} alt={details.alt} className='item-image' />
-    <span>{details.cost}</span>
-    <label htmlFor={details.id}>Quantity (between 1 and 10):</label>
+    <div>{details.cost}</div>
+    <div className='item-sub'>Sizes</div>
+    <ul className='sizes'>
+      {details.sizes.map((size, index) =>
+        <button key={index} className='size'>{size}</button>
+      )}
+    </ul>
+    <label className='item-sub' htmlFor={details.id}>Quantity (between 1 and 10):</label>
     <input type='number' id={details.id} name='quantity' min={1} max={10} />
     <button className='add-to-cart'>Add To Cart</button>
     </>
