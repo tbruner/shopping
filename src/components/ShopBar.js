0a380030955ef90bb1972554610ddf3b29da0ca6
tbruner <trevor.a.bruner@gmail.com>
2022-12-09 19:22:12 -0500

    Format total as currency

--- src/components/ShopBar.js
+++ src/components/ShopBar.js
@@ -4,7 +4,7 @@ const ShopBar = ({ itemCount, saleTotal }) => {
   return (
     <div className='sticky'>
       <span className='item-count'>Count: {itemCount}</span>
-      <span className='total-cost'>Total: {saleTotal}</span>
+      <span className='total-cost'>Total: ${saleTotal.toFixed(2)}</span>
       <button className='checkout-btn'>Checkout</button>
     </div>
   );
