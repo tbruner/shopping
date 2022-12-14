ecb65a7c611d1171f813de463f0c6271de25cf3f
tbruner <trevor.a.bruner@gmail.com>
2022-12-13 20:59:36 -0500

    Make size selection radio buttons

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -7,11 +7,15 @@ const ShopItem = ({details, addToCart}) => {
     <img src={details.image} alt={details.alt} className='item-image' />
     <div>{details.cost}</div>
     <div className='item-sub'>Sizes</div>
-    <ul className='sizes'>
+    <fieldset className='sizes'>
+      <label>Please select a size:</label>
       {details.sizes.map((size, index) =>
-        <button key={index} className='size'>{size}</button>
+        <div  key={size + details.id.toString()}>
+        <input type='radio' id={size} value={size} name='size' autoComplete='off' />
+        <label className='size' htmlFor={size}>{size}</label>
+        </div>
       )}
-    </ul>
+    </fieldset>
     <label className='item-sub' htmlFor={details.id}>Quantity (between 1 and 10):</label>
     <input type='number' id={details.id} name='quantity' min={1} max={10} />
     <button className='add-to-cart' onClick={() => addToCart(details.id)}>Add To Cart</button>
