119a4e7c04ccf2d21362e00fba9370c5ac578119
tbruner <trevor.a.bruner@gmail.com>
2022-12-16 19:44:57 -0500

    Style Shop Bar

--- src/components/ShopBar.js
+++ src/components/ShopBar.js
@@ -3,9 +3,9 @@ import '../styles/ShopBar.css';
 const ShopBar = ({ itemCount, saleTotal }) => {
   return (
     <div className='sticky'>
-      <span className='item-count'>Count: {itemCount}</span>
-      <span className='total-cost'>Total: ${saleTotal.toFixed(2)}</span>
-      <button className='checkout-btn'>Checkout</button>
+      <span className='item-count sticky-bar-itm'>Count: {itemCount}</span>
+      <span className='total-cost sticky-bar-itm'>Total: ${saleTotal.toFixed(2)}</span>
+      <button className='checkout-btn sticky-bar-itm'>Checkout</button>
     </div>
   );
 }
