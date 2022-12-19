a53ae2e265274c4505c52f9db5b9391a914f219a
tbruner <trevor.a.bruner@gmail.com>
2022-12-18 20:34:02 -0500

    Add content to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,26 @@ import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <>
-      <h1>Check out these "wild" new styles!</h1>
-    </>
+    <div className='home'>
+      <h1>Browse our "wild" selection of shoes!</h1>
+      <div className='home-promo'>
+        <div className='item-promo'>
+          Nike
+        </div>
+        <div className='item-promo'>
+          Under Armor
+        </div>
+        <div className='item-promo'>
+          UGG
+        </div>
+        <div className='item-promo'>
+          Sperry
+        </div>
+      </div>
+      <footer>
+        Created by Trevor Bruner
+      </footer>
+    </div>
   );
 }
 
