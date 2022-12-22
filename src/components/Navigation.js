0ddb4292d555d59421286c38cf1e9bc59a8c8112
tbruner <trevor.a.bruner@gmail.com>
2022-12-21 19:16:26 -0500

    Add logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import '../styles/Navigation.css';
+import logo from '../images/shoeZooLogo.svg';
 
 const Navigation = () => {
   return (
     <nav>
-      <Link className='nav-link' to='/'>Logo tbd</Link>
+      <Link className='nav-link' to='/'><img className='logo' src={logo} alt='Shoe Zoo logo' /></Link>
       <Link className='nav-link' to='/mens'>Mens</Link>
       <Link className='nav-link' to='/womens'>Womens</Link>
       <Link className='nav-link' to='/kids'>Kids</Link>
